283a091eadb01ccf4799161fbb80d9c6d1e7778c
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-10-25 13:22:02 -0700

    finalized models

--- models/User.js
+++ models/User.js
@@ -1,22 +1,44 @@
-const { Schema, Types } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema(
+const userSchema = new Schema(
     {
-        userId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
         username: {
             type: String,
             unique: true,
             required: true,
-            trimmed: true,
+            trim: true,
         },
         email: {
             type: String,
             required: true,
             unique: true,
-            
-        }
+            match: [/^(\w\.-+)@([\w\.-]+)\.([\w]{2,})$/],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+userSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
